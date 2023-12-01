3d0582af034012718de4d3ccef7371b4a9d20e9b
Paddewoo <paddewoo@gmail.com>
2023-12-01 13:23:40 +0100

    j

--- Sudoku.java
+++ Sudoku.java
@@ -1,7 +1,20 @@
-public class Sudoku {
+public class Sudoku{
     private int[][] matrix;
 
+    /* Constructor */
     public Sudoku(){
         matrix = new int[9][9];
     }
+
+    /* Sets the value of the board */
+    public void setValue(){
+
+    }
+
+    /* Get the value of the board */
+    public int getValue(){
+
+        return 0;
+    }
+
 }
