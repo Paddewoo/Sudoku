2bc26768d6ad8fcd1b3aa2235b106093b0376605
Paddewoo <paddewoo@gmail.com>
2023-12-01 15:17:28 +0100

    j

--- Sudoku.java
+++ Sudoku.java
@@ -1,5 +1,4 @@
 
-
 import javax.swing.JFrame;
 
 public class Sudoku implements SudokuSolverInterface{
@@ -8,21 +7,20 @@ public class Sudoku implements SudokuSolverInterface{
     /* Constructor */
     public Sudoku(){
         grid = new int[9][9];
-
-    }
-
-    /**
-     * Set sudoku board, numbers 1-9 are fixed values, 0 is unsolved. 
-     */
-    @Override
-    public void setBoard(int[][] board) {
-        grid = board;
-        for(int r = 0; r < 9; r++){
+        
+        for(int r = 0; r < 9; r++){         // startar med alla noll
             for(int c = 0; c < 9; c++){
                 grid[r][c] = 0;
             }
         }
+    }
 
+    /**
+     * Set sudoku board, numbers 1-9 are fixed values, 0 is unsolved.
+     */
+    @Override
+    public void setBoard(int[][] board) {
+        grid = board;
     }
 
     /**
--- SudokuApplication.java
+++ SudokuApplication.java
@@ -1,6 +1,9 @@
-import javax.swing.*;
 
 public class SudokuApplication {
-    
-
+    public static void main(String[] args) {
+        int[][] board = new int[9][9]; 
+        Sudoku s = new Sudoku();
+        s.setBoard(board);
+        
+    }
 }
