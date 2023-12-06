633be61004e73fef58f49a0078d93e0a49e5ad83
Paddewoo <paddewoo@gmail.com>
2023-12-06 11:57:11 +0100

    j

--- SudokuApplication.java
+++ SudokuApplication.java
@@ -3,7 +3,8 @@ public class SudokuApplication {
     public static void main(String[] args) {
         int[][] board = new int[9][9]; 
         Sudoku s = new Sudoku();
-        s.setBoard(board);
+        SudokuView view = new SudokuView(s, "Sudoku", 500, 500);
+        //s.setBoard(board);
         
     }
 }
