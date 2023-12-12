e8439b1f7d0bdd1adaffb1d5acd98411533a620c
Paddewoo <paddewoo@gmail.com>
2023-12-12 10:23:16 +0100

    testade ändra testFig1

--- Test/SudokuTester.java
+++ Test/SudokuTester.java
@@ -78,6 +78,55 @@ public class SudokuTester {
     }
 
     @Test
+    void testFig1(){
+
+        // board = {
+        //     {0, 0, 8, 0, 0, 9, 0, 6, 2},
+        //     {0, 0, 0, 0, 0, 0, 0, 0, 5},
+        //     {1, 0, 2, 5, 0, 0, 0, 0, 0},
+
+        // };
+
+        board[0][2] = 8;
+        board[0][5] = 9;
+        board[0][7] = 6;
+        board[0][8] = 2;
+
+        board[1][8] = 5;
+
+        board[2][0] = 1;
+        board[2][2] = 2;
+        board[2][3] = 5;
+
+        board[3][3] = 2;
+        board[3][4] = 1;
+        board[3][7] = 9;
+
+        board[4][1] = 5;
+        board[4][6] = 6;
+
+        board[5][0] = 6;
+        board[5][7] = 2;
+        board[5][8] = 8;
+
+        board[6][0] = 4;
+        board[6][1] = 1;
+        board[6][3] = 6;
+        board[6][5] = 8;
+
+        board[7][0] = 8;
+        board[7][1] = 6;
+        board[7][4] = 3;
+        board[7][6] = 1;
+
+        board[8][6] = 4;
+
+        sudoku.setBoard(board);
+
+        assertTrue(sudoku.solve());
+    }
+
+    @Test
     void testIsLegal(){
         assertTrue(sudoku.isLegal(0, 0, 1));
         sudoku.set(0, 0, 1);
